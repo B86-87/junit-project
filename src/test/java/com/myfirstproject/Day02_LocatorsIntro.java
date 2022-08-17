6fb5e4a6ccaa2fe579b3fae620b92a71c9d647b7
techproed <abayram.techproed@gmail.com>
2022-08-16 20:48:22 -0500

    day03

--- src/test/java/com/myfirstproject/Day02_LocatorsIntro.java
+++ src/test/java/com/myfirstproject/Day02_LocatorsIntro.java
@@ -1,18 +1,36 @@
 package com.myfirstproject;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
 
 public class Day02_LocatorsIntro {
 
+    WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+//        Adding implicitly wait to help wait issue
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
     @Test
     public void login(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
 
 //        going to the URL
         driver.get("http://a.testaddressbook.com/sign_in");
@@ -26,9 +44,48 @@ public class Day02_LocatorsIntro {
 //        Location the Submit Button
         driver.findElement(By.name("commit")).click();
 
-//        close the driver
-        driver.quit();
+//        Then verify that the expected user id testtechproed@gmail.com
+//        (USE getText() method to get the text from the page)
+        String actualIdText = driver.findElement(By.className("navbar-text")).getText();
+        String expectedIdText = "testtechproed@gmail.com";
+        Assert.assertEquals(expectedIdText,actualIdText);
 
+//        Verify the Addresses and Sign Out texts are displayed
+        WebElement addressElement = driver.findElement(By.linkText("Addresses"));
+        Assert.assertTrue(addressElement.isDisplayed());
+
+        WebElement actualSignOutLink = driver.findElement(By.linkText("Sign out"));
+        Assert.assertTrue(actualSignOutLink.isDisplayed());
+
+//        Find the number of total link on the page
+//        a tag is used to create link
+        List<WebElement> linkList=driver.findElements(By.tagName("a"));//returns the total number of link on the page
+        int numOfLink = linkList.size();
+        System.out.println("Number of link : "+numOfLink);
+
+//        Print all link names
+        for (WebElement eachElement: linkList){
+            System.out.println(eachElement.getText());
+        }
+
+//        Sign out from the page
+        driver.findElement(By.xpath("//a[@data-test='sign-out']")).click();
+
+//        We are able to click on the signout button
+//        We need to assert the sign out functionality works
+//        We can locate a core element for assertion
+//        Or we can use URL for assertion
+//        Sign out URL: http://a.testaddressbook.com/
+//        Assert that after click on signout, the URL should be http://a.testaddressbook.com/sign_in
+
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertTrue(currentUrl.equals("http://a.testaddressbook.com/sign_in"));
+
+
+        /*
+        * getText() is used to get teh text of a webelement. This returns a string
+        * isDisplayed() is used to check if an element is on the page or nor. This returns a boolean
+        * */
     }
 
 }
---
+++ src/test/java/com/myfirstproject/Day03_Checkbox.java
@@ -0,0 +1,52 @@
+package com.myfirstproject;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class Day03_Checkbox {
+    WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    public void checkboxTest(){
+//        Go to https://the-internet.herokuapp.com/checkboxes
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+//        Locate the elements of checkboxes
+        WebElement checkbox1 = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]"));
+//        Then click on checkbox1 if box is not selected
+        if (!checkbox1.isSelected()){
+            checkbox1.click();
+        }
+//        Then click on checkbox2 if box is not selected
+        if (!checkbox2.isSelected()){
+            checkbox2.click();
+        }
+//        Then verify that checkbox1 is checked.
+        Assert.assertTrue(checkbox1.isSelected());
+    }
+
+    /*
+    * isSelected() is used to check if a checkbox or radio button is selected
+    * If a checkbox is selected then this returns true
+    * Is not selected then returns false
+    * */
+}
